b97623685d2e1b0b4d1dd5219c8554b216ea9df0
jaredkrinke <jaredkrinke@gmail.com>
2014-04-27 15:36:32 -0700

    Basic transient functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,65 @@
 ﻿$(function () {
+    // Events
+    var balanceUpdated = function () { };
+    var transactionAdded = function () { };
+
+    // Data model
+    // TODO: Load from local storage
+    var balance = 0;
+    var transactions = [];
+
+    var addTransactionInternal = function (transaction) {
+        transactions.unshift(transaction);
+        balance -= transaction.amount;
+    };
+
+    var addTransaction = function (transaction) {
+        addTransactionInternal(transaction);
+
+        // TODO: Persist changes to local storage
+
+        balanceUpdated(balance);
+        transactionAdded(transaction);
+    };
+
+    // UI
     var template = $('#transaction-template').hide();
 
     $('#add').click(function () {
         // TODO: Validation
+        addTransaction({
+            description: $('#add-description').val(),
+            amount: +$('#add-amount').val(),
+        });
+    });
+
+    // Bind UI to data model
+    // TODO: Currency formatting and symbol
+    var balanceText = $('#balance');
+    var balanceStatus = $('#balance-status');
+    balanceUpdated = function (balance) {
+        balanceText.text(balance);
+
+        var statusClass = 'panel-success';
+        if (balance < 0) {
+            statusClass = 'panel-danger';
+        } else if (balance < 50) {
+            statusClass = 'panel-warning';
+        }
+        balanceStatus.removeClass('panel-success panel-warning panel-danger').addClass(statusClass);
+    };
+
+    transactionAdded = function (transaction) {
         template.clone()
             .insertAfter(template)
             .show()
-            .find('.transaction-description').text($('#add-description').val()).end()
-            .find('.transaction-amount').text($('#add-amount').val()).end();
-    });
+            .find('.transaction-description').text(transaction.description).end()
+            .find('.transaction-amount').text(transaction.amount).end();
+    };
+
+    // Initial state
+    balanceUpdated(balance);
+
+    // TODO: Deleting transactions
+    // TODO: Automate monthly addition of funds
 });
