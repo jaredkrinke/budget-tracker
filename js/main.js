f35affc508200e66127a23d1127c763f61ac71e0
jaredkrinke <jaredkrinke@gmail.com>
2014-04-27 14:59:17 -0700

    Basic add functionality

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    var template = $('#transaction-template').hide();
+
+    $('#add').click(function () {
+        // TODO: Validation
+        template.clone()
+            .insertAfter(template)
+            .show()
+            .find('.transaction-description').text($('#add-description').val()).end()
+            .find('.transaction-amount').text($('#add-amount').val()).end();
+    });
+});
